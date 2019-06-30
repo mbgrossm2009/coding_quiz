83fc9caaa1bf0c9794e94b85562f29802efe72d5
Matthew Grossman <grossmanmatt@yahoo.com>
2019-06-30 18:48:07 -0400

    showAlert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from "react";
 import Question from "./Components/Question";
 import Quiz from "./Components/Quiz";
 import AnswerOption from "./Components/AnswerOption";
@@ -18,7 +18,8 @@ class App extends React.Component {
       answersCount: {},
       rightAnswers: 0,
       wrongAnswers: 0,
-      correct: false
+      correct: null,
+      showAlert:false
     };
   }
 
@@ -31,58 +32,54 @@ class App extends React.Component {
   }
 
   handleAnswerSelected(event) {
-
     for (var i = 0; i < quizQuestions.length; i++) {
-      const isCorrect = (guess) => {
-
+      const isCorrect = guess => {
         var theCorrectAnswer = guess.answers[i].isTheAnswer === true;
         var theCorrectAnswerContent = guess.answers[i].content;
-          if (theCorrectAnswer && theCorrectAnswerContent === event.currentTarget.value) {
+        if (theCorrectAnswer && theCorrectAnswerContent === event.currentTarget.value) {
           console.log("Correct");
           this.setState({
-          rightAnswers: this.state.rightAnswers + 1
-          })
-          console.log(this.state.rightAnswers)
-
+            rightAnswers: this.state.rightAnswers + 1,
+            correct: <Correctness theme = "alert alert-success" answer = "Correct"/>,
+            showAlert: true
+          });
         } else if (!theCorrectAnswer && theCorrectAnswerContent === event.currentTarget.value) {
           console.log("Incorrect");
           this.setState({
-          wrongAnswers: this.state.wrongAnswers + 1
-          })
-          console.log(this.state.wrongAnswers)
-
-
+            wrongAnswers: this.state.wrongAnswers + 1,
+            correct: <Correctness theme = "alert alert-danger" answer = "Wrong"/>,
+            showAlert: true
+          });
+          console.log(this.state.correct)
         }
-
-      }
+      };
       quizQuestions.find(isCorrect);
-
     }
 
+    if (this.state.questionId < quizQuestions.length) {
+      setTimeout(() => this.setNextQuestion(), 300);
+    }
+  }
+    addCorrectAnswer(rightAnswers){
+      this.setState({
+        rightAnswers: this.state.rightAnswers + 1
+      })
+    
+    }
 
-  // if (this.state.questionId < quizQuestions.length) {
-  //   setTimeout(() => this.setNextQuestion(), 300);
-  // }
-
-  // addCorrectAnswer(rightAnswers){
-  //   this.setState({
-  //     rightAnswers: this.state.rightAnswers + 1
-  //   })
-  //
-  // }
-
-  // setNextQuestion() {
-  //   const counter = this.state.counter + 1;
-  //   const questionId = this.state.questionId + 1;
-  //   this.setState({
-  //     counter: counter,
-  //     questionId: questionId,
-  //     question: quizQuestions[counter].question,
-  //     answerOptions: quizQuestions[counter].answers,
-  //     answer: ""
-  //   });
-  // }
-}
+    setNextQuestion() {
+      const counter = this.state.counter + 1;
+      const questionId = this.state.questionId + 1;
+      this.setState({
+        counter: counter,
+        questionId: questionId,
+        question: quizQuestions[counter].question,
+        answerOptions: quizQuestions[counter].answers,
+        answer: "",
+        showAlert: false
+      });
+    }
+  
 
   render() {
     return (
@@ -95,7 +92,9 @@ class App extends React.Component {
           questionTotal={quizQuestions.length}
           onAnswerSelected={this.handleAnswerSelected}
         />
+    {this.state.showAlert ? this.state.correct : null}
       </div>
+
     );
   }
 }
